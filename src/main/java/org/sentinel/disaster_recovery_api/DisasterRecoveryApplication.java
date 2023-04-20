61db3352e00ce676a4f53dcb008f7cb62a9051d2
NBryant83 <73191760+NBryant83@users.noreply.github.com>
2023-04-20 14:54:43 -0400

    completed base dependencies

---
+++ src/main/java/org/sentinel/disaster_recovery_api/DisasterRecoveryApplication.java
@@ -0,0 +1,13 @@
+package org.sentinel.disaster_recovery_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisasterRecoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisasterRecoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sentinel/disaster_recovery_api/DisasterRecoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sentinel.disaster_recovery_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisasterRecoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
